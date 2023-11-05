34d491c3dee0e0672363a5c842fb75d7f812719d
albik123 <127779749+albik123@users.noreply.github.com>
2023-11-05 09:59:10 +0100

    Update ResourceManager.hpp

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,26 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+private:
+    Resource resource; // Zarządzany obiekt typu Resource
+
+public:
+    // Konstruktor domyślny inicjalizujący zarządzany obiekt
+    ResourceManager() : resource() {}
+
+    // Metoda get, która zwraca wynik zawołania metody get obiektu Resource
+    double get() {
+        return resource.get();
+    }
+
+    // Konstruktor kopiujący
+    ResourceManager(const ResourceManager& other) : resource(other.resource) {}
+
+    // Operator przypisania
+    ResourceManager& operator=(const ResourceManager& other) {
+        if (this != &other) {
+            resource = other.resource;
+        }
+        return *this;
+    }
 };
